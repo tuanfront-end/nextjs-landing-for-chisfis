5baebcb62426143bfccb213f59c955fd6c302de5
Chis Nghiax <atuanvptd@gmail.com>
2026-04-08 15:18:26 +0700

    update st

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -96,7 +96,7 @@ function MobileNavigation() {
 
 export function Header() {
   return (
-    <header className="py-10">
+    <header className="py-6">
       <Container>
         <nav className="relative z-50 flex justify-between">
           <div className="flex items-center md:gap-x-12">
